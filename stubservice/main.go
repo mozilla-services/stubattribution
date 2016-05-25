98ebc88928bceb576fa98c655b609d18d930ed04
Jeremiah Orem <oremj@oremj.com>
2016-05-25 09:21:54 -0700

    Add environment variables

--- stubservice/main.go
+++ stubservice/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/mozilla-services/go-stubattribution/stubservice/stubhandlers"
 )
 
-func main() {
+var RETURN_MODE = os.Getenv("RETURN_MODE")
 
-	log.Fatal(http.ListenAndServe("127.0.0.1:8000", http.HandlerFunc(stubhandlers.StubHandler)))
+var S3_BUCKET = os.Getenv("S3_BUCKET")
+var S3_PREFIX = os.Getenv("S3_PREFIX")
+
+var CDN_PREFIX = os.Getenv("CDN_PREFIX")
+
+var ADDR = os.Getenv("ADDR")
+
+func init() {
+	switch RETURN_MODE {
+	case "redirect":
+		RETURN_MODE = "redirect"
+	default:
+		RETURN_MODE = "direct"
+	}
+
+	if CDN_PREFIX == "" {
+		CDN_PREFIX = fmt.Sprintf("https://s3.amazonaws.com/%s/", S3_BUCKET)
+	}
+
+	if ADDR == "" {
+		ADDR = "127.0.0.1:8000"
+	}
+}
+
+func main() {
+	stubHandler := &stubhandlers.StubHandler{
+		ReturnMode: RETURN_MODE,
+		CDNPrefix:  CDN_PREFIX,
+		S3Bucket:   S3_BUCKET,
+		S3Prefix:   S3_PREFIX,
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", stubHandler)
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8000", mux))
 }
--- stubservice/stubhandlers/stubhandler.go
+++ stubservice/stubhandlers/stubhandler.go
@@ -16,10 +16,7 @@ import (
 	"github.com/mozilla-services/go-stubattribution"
 )
 
-var RETURN_METHOD = "redirect"
 var BOUNCER_URL = "https://download.mozilla.org/"
-var S3_BUCKET = "net-mozaws-stage-us-east-1-stub-attribution"
-var CDN_PREFIX = fmt.Sprintf("https://s3.amazonaws.com/%s/", S3_BUCKET)
 
 func uniqueKey(downloadURL, attributionCode string) string {
 	hasher := sha256.New()
@@ -30,117 +27,162 @@ func uniqueKey(downloadURL, attributionCode string) string {
 func bouncerURL(product, lang, os string) string {
 	v := url.Values{}
 	v.Set("product", product)
-	if lang != "" {
-		v.Set("lang", lang)
-	}
-	if os != "" {
-		v.Set("os", os)
-	}
+	v.Set("lang", lang)
+	v.Set("os", os)
 	return BOUNCER_URL + "?" + v.Encode()
 }
 
-func fetchModifyStub(url, attributionCode string) ([]byte, error) {
+type modifiedStub struct {
+	Data []byte
+	Resp *http.Response
+}
+
+func fetchModifyStub(url, attributionCode string) (*modifiedStub, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("fetchModifyStub: http.Get%v", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("fetchModifyStub: %v", err)
 	}
 
-	if attributionCode == "" {
-		return body, nil
+	if attributionCode != "" {
+		data, err = stubattribution.WriteAttributionCode(data, []byte(attributionCode))
+		if err != nil {
+			return nil, fmt.Errorf("fetchModifyStub: %v", err)
+		}
 	}
+	return &modifiedStub{
+		Data: data,
+		Resp: resp,
+	}, nil
 
-	data, err := stubattribution.WriteAttributionCode(body, []byte(attributionCode))
-	if err != nil {
-		return nil, fmt.Errorf("fetchModifyStub: %v", err)
-	}
-	return data, nil
 }
 
-func StubHandler(w http.ResponseWriter, r *http.Request) {
-	product := "Firefox-46.0-Stub"
-	lang := "en-US"
-	os := "win"
-	attributionCode := ""
+type StubHandler struct {
+	ReturnMode string
 
-	req, err := http.NewRequest("GET",
-		bouncerURL(product, lang, os), nil)
+	CDNPrefix string
+
+	S3Bucket string
+	S3Prefix string
+}
+
+// redirectResponse returns "", nil if not found
+func redirectResponse(url string) (string, error) {
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("StubHandler: NewRequest: %v", err)
-		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
-		return
+		return "", fmt.Errorf("StubHandler: NewRequest: %v", err)
 	}
 
 	resp, err := http.DefaultTransport.RoundTrip(req)
 	if err != nil {
-		log.Printf("StubHandler: RoundTrip: %v", err)
-		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
-		return
+		return "", fmt.Errorf("RoundTrip: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 302 || resp.Header.Get("Location") == "" {
+		return "", nil
+	}
+
+	return resp.Header.Get("Location"), nil
+}
+
+func (s *StubHandler) ServeDirect(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	product := query.Get("product")
+	lang := query.Get("lang")
+	os := query.Get("os")
+	attributionCode := query.Get("attribution_code")
+
+	cdnURL, err := redirectResponse(bouncerURL(product, lang, os))
+	if err != nil {
+		log.Printf("StubHandler: redirectResponse: %v", err)
+		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+		return
+	}
+	if cdnURL == "" {
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+	stub, err := fetchModifyStub(cdnURL, attributionCode)
+	if err != nil {
+		log.Printf("StubHandler: %v", err)
+		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", stub.Resp.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(stub.Data)))
+	w.Write(stub.Data)
+}
+
+func (s *StubHandler) ServeRedirect(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	product := query.Get("product")
+	lang := query.Get("lang")
+	os := query.Get("os")
+	attributionCode := query.Get("attribution_code")
+
+	cdnURL, err := redirectResponse(bouncerURL(product, lang, os))
+	if err != nil {
+		log.Printf("StubHandler: redirectResponse: %v", err)
+		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+		return
+	}
+	if cdnURL == "" {
 		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
 
-	cdnURL := resp.Header.Get("Location")
-	if RETURN_METHOD == "direct" {
-		data, err := fetchModifyStub(cdnURL, attributionCode)
-		if err != nil {
-			log.Printf("StubHandler: %v", err)
-			http.Error(w, "Internal Service Error", http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-		w.Header().Set("Content-Length", fmt.Sprintf("%d", len(data)))
-		w.Write(data)
+	filename, err := url.QueryUnescape(path.Base(cdnURL))
+	if err != nil {
+		log.Printf("StubHandler: %v", err)
+		http.Error(w, "Internal Service Error", http.StatusInternalServerError)
 		return
-	} else if RETURN_METHOD == "redirect" {
-		filename, err := url.QueryUnescape(path.Base(cdnURL))
+	}
+
+	s3Key := (s.S3Prefix + "builds/" +
+		product + "/" +
+		lang + "/" +
+		os + "/" +
+		uniqueKey(cdnURL, attributionCode) + "/" +
+		filename)
+
+	s3Svc := s3.New(session.New())
+	_, err = s3Svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(s.S3Bucket),
+		Key:    aws.String(s3Key),
+	})
+	if err != nil {
+		stub, err := fetchModifyStub(cdnURL, attributionCode)
 		if err != nil {
 			log.Printf("StubHandler: %v", err)
 			http.Error(w, "Internal Service Error", http.StatusInternalServerError)
 			return
 		}
-
-		s3Key := ("builds/" +
-			product + "/" +
-			lang + "/" +
-			os + "/" +
-			uniqueKey(cdnURL, attributionCode) + "/" +
-			filename)
-
-		s3Svc := s3.New(session.New())
-		_, err = s3Svc.HeadObject(&s3.HeadObjectInput{
-			Bucket: aws.String(S3_BUCKET),
-			Key:    aws.String(s3Key),
-		})
-		if err != nil {
-			data, err := fetchModifyStub(cdnURL, attributionCode)
-			if err != nil {
-				log.Printf("StubHandler: %v", err)
-				http.Error(w, "Internal Service Error", http.StatusInternalServerError)
-				return
-			}
-			putObjectParams := &s3.PutObjectInput{
-				Bucket:      aws.String(S3_BUCKET),
-				Key:         aws.String(s3Key),
-				ContentType: aws.String(resp.Header.Get("Content-Type")),
-				Body:        bytes.NewReader(data),
-			}
-			_, err = s3Svc.PutObject(putObjectParams)
-			if err != nil {
-				log.Printf("StubHandler: PutObject %v", err)
-				http.Error(w, "Internal Service Error", http.StatusInternalServerError)
-				return
-			}
+		putObjectParams := &s3.PutObjectInput{
+			Bucket:      aws.String(s.S3Bucket),
+			Key:         aws.String(s3Key),
+			ContentType: aws.String(stub.Resp.Header.Get("Content-Type")),
+			Body:        bytes.NewReader(stub.Data),
 		}
-		http.Redirect(w, req, CDN_PREFIX+s3Key, http.StatusTemporaryRedirect)
+		_, err = s3Svc.PutObject(putObjectParams)
+		if err != nil {
+			log.Printf("StubHandler: PutObject %v", err)
+			http.Error(w, "Internal Service Error", http.StatusInternalServerError)
+			return
+		}
 	}
+	http.Redirect(w, req, s.CDNPrefix+s3Key, http.StatusTemporaryRedirect)
+}
+
+func (s *StubHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if s.ReturnMode == "redirect" {
+		s.ServeRedirect(w, req)
+		return
+	}
+	s.ServeDirect(w, req)
 }
