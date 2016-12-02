cff61d9edd4b2764561f39f23d814daffcd86306
Jeremiah Orem <oremj@oremj.com>
2016-12-02 15:50:38 -0800

    stubhandler: add full e2e redirect test

--- stubservice/stubhandlers/stubhandler_test.go
+++ stubservice/stubhandlers/stubhandler_test.go
@@ -1,14 +1,19 @@
 package stubhandlers
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 	"testing/quick"
+
+	"github.com/mozilla-services/stubattribution/stubservice/backends"
 )
 
 func TestValidateSignature(t *testing.T) {
@@ -158,4 +163,81 @@ func TestRedirectResponse(t *testing.T) {
 			t.Errorf("Incorrect error: %v", err)
 		}
 	})
+}
+
+func TestRedirectFull(t *testing.T) {
+	testFileBytes, err := ioutil.ReadFile("../../testdata/test-stub.exe")
+	if err != nil {
+		t.Fatal("could not read test-stub.exe", err)
+	}
+
+	storage := backends.NewMapStorage()
+
+	var server *httptest.Server
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		switch req.URL.Path {
+		case "/":
+			http.Redirect(w, req, server.URL+"/thefile", 302)
+			return
+		case "/thefile":
+			w.Write(testFileBytes)
+			return
+		}
+		if strings.HasPrefix(req.URL.Path, "/cdn/") {
+			item, ok := storage.Storage[strings.TrimPrefix(req.URL.Path, "/cdn/")]
+			if !ok {
+				w.WriteHeader(404)
+				return
+			}
+			w.Header().Set("Content-Type", item.ContentType)
+			w.Write(item.Bytes)
+			return
+		}
+	})
+	server = httptest.NewServer(handler)
+	defer server.Close()
+
+	BouncerURL = server.URL
+	defer func() {
+		BouncerURL = "https://download.mozilla.org/"
+	}()
+
+	svc := &StubService{
+		Handler: &StubHandlerRedirect{
+			CDNPrefix: server.URL + "/cdn/",
+			Storage:   storage,
+			KeyPrefix: "",
+		},
+	}
+
+	recorder := httptest.NewRecorder()
+	attributionCode := "campaign=%28not+set%29&content=%28not+set%29&medium=organic&source=www.google.com"
+	req := httptest.NewRequest("GET", `http://test/?product=firefox-stub&os=win&lang=en-US&attribution_code=`+url.QueryEscape(attributionCode), nil)
+	svc.ServeHTTP(recorder, req)
+
+	if recorder.HeaderMap.Get("Location") == "" {
+		t.Fatal("Location is not set")
+	}
+
+	resp, err := http.Get(recorder.HeaderMap.Get("Location"))
+	if err != nil {
+		t.Fatal("request failed", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatalf("request was not 200 res: %d", resp.StatusCode)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("could not read body", err)
+	}
+
+	if len(bodyBytes) != len(testFileBytes) {
+		t.Error("Returned file was not the same length as the original file")
+	}
+
+	if !bytes.Contains(bodyBytes, []byte(attributionCode)) {
+		t.Error("Returned file did not contain attribution code")
+	}
 }
