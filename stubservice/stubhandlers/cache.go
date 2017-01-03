59f74298a1f52e6f557e3293959501c2d9bd09ce
Jeremiah Orem <oremj@oremj.com>
2017-01-03 10:07:56 -0800

    cache: add a global stub cache

--- stubservice/stubhandlers/cache.go
+++ stubservice/stubhandlers/cache.go
@@ -7,9 +7,23 @@ import (
 	"github.com/mozilla-services/stubattribution/cache"
 )
 
+var globalStubCache = newStubCache(
+	1024*1024*1000, // 1G
+	5*time.Minute,
+)
+
 type stub struct {
 	contentType string
 	body        []byte
+}
+
+func (s *stub) copy() *stub {
+	b := make([]byte, len(s.body))
+	copy(b, s.body)
+	return &stub{
+		contentType: s.contentType,
+		body:        b,
+	}
 }
 
 type stubCache struct {
@@ -25,6 +39,7 @@ func newStubCache(maxSize int64, dur time.Duration) *stubCache {
 	}
 }
 
+// Add adds a new stub to the cache
 func (s *stubCache) Add(key string, st *stub) {
 	s.lck.Lock()
 	defer s.lck.Unlock()
@@ -34,12 +49,20 @@ func (s *stubCache) Add(key string, st *stub) {
 	s.lru.Add(key, st, size, expires)
 }
 
+// Get returns a copy, if it exists so the original data is never corrupted
 func (s *stubCache) Get(key string) *stub {
 	s.lck.Lock()
 	defer s.lck.Unlock()
 
 	if val, hit := s.lru.Get(key); hit {
-		return val.(*stub)
+		return val.(*stub).copy()
 	}
 	return nil
+}
+
+// SetMaxSize sets a new maxsize, but does not resize the underlying cache
+func (s *stubCache) SetMaxSize(maxSize int64) {
+	s.lck.Lock()
+	defer s.lck.Unlock()
+	s.lru.MaxSize = maxSize
 }
