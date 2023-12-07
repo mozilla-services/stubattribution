41f884b018827abb22d8e214a7d3a64b6b6258b1
Heitor Neiva <hneiva@mozilla.com>
2023-12-06 16:49:59 -0800

    Refactor utils_test
    Improve error messages
    Make it so it only reads test DMG and EXE files once

--- stubservice/stubhandlers/utils_test.go
+++ stubservice/stubhandlers/utils_test.go
@@ -1,19 +1,22 @@
 package stubhandlers
 
 import (
+	"bytes"
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"reflect"
 	"testing"
 
 	"github.com/mozilla-services/stubattribution/dmglib"
 )
 
+const attributionChars = "abcdefghijklmnopqrstuvwxyz1234567890"
+
 func TestFetchStub(t *testing.T) {
 	t.Run("fetchStub", func(t *testing.T) {
-		sampleBody := []byte("HelloWorld")
+		// Empty json object
+		sampleBody := []byte(`{"hello": "world"}`)
 		// Create a server that returns a static JSON response
 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Write(sampleBody)
@@ -21,33 +24,32 @@ func TestFetchStub(t *testing.T) {
 		defer s.Close()
 		got, err := fetchStub(s.URL)
 		if err != nil {
-			t.Errorf("fetchStub error: %s", err)
+			t.Errorf("Unexpected error: %s", err)
 		}
-		if want := string(sampleBody); want != string(got.body) {
-			t.Errorf("Expected %s, got: %s", want, got)
+		if !bytes.Equal(got.body, sampleBody) {
+			t.Errorf("Expected %s, got: %s", sampleBody, got.body)
 		}
 	})
 
-	t.Run("fetchStub Get error", func(t *testing.T) {
+	t.Run("fetchStub - invalid URL", func(t *testing.T) {
 		errMessage := `Get: Get "bogus://url": unsupported protocol scheme "bogus"`
 		_, err := fetchStub("bogus://url")
 		if err == nil {
-			t.Error("expected an error with bogus URL!")
+			t.Error("Expected an error with bogus URL")
 		}
 		if err.Error() != errMessage {
 			t.Errorf("Expected %s, got: %s", errMessage, err.Error())
 		}
 	})
 
-	t.Run("fetchStub StatusCode", func(t *testing.T) {
-		// Create a server that returns a static JSON response
+	t.Run("fetchStub - non-OK status code", func(t *testing.T) {
 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusBadRequest)
 		}))
 		defer s.Close()
 		_, err := fetchStub(s.URL)
 		if err == nil {
-			t.Error("expected an error with invalid status code!")
+			t.Error("Expected an error with invalid status code")
 		}
 		if err.Error() != "invalid status code" {
 			t.Errorf("Expected 'invalid status code', got: %s", err.Error())
@@ -55,9 +57,7 @@ func TestFetchStub(t *testing.T) {
 	})
 }
 
-const attributionChars = "abcdefghijklmnopqrstuvwxyz1234567890"
-
-func _randomString(length int) string {
+func makeRandomString(length int) string {
 	str := make([]byte, length)
 	for i := range str {
 		str[i] = attributionChars[rand.Intn(len(attributionChars))]
@@ -71,89 +71,73 @@ func TestModifyStubEXE(t *testing.T) {
 		t.Errorf("Error reading test EXE: %s", err)
 	}
 
-	st := &stub{
-		body: fileBytes,
-	}
+	t.Run("modifyStub - EXE success", func(t *testing.T) {
+		st := &stub{
+			body: fileBytes,
+		}
+		_, err = modifyStub(st, "hello=attribution&os=win", "win")
+		if err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+	})
 
-	_, err = modifyStub(st, "hello=attribution&os=win", "win")
-	if err != nil {
-		t.Errorf("modifyStub error: %s", err)
-	}
-}
+	t.Run("modifyStub - EXE fail", func(t *testing.T) {
+		st := &stub{
+			body: fileBytes,
+		}
+		attribution := makeRandomString(100000)
 
-func TestModifyStubEXEFailWrite(t *testing.T) {
-	fileBytes, err := os.ReadFile("../../testdata/test-stub.exe")
-	if err != nil {
-		t.Errorf("Error reading test EXE: %s", err)
-	}
-
-	st := &stub{
-		body: fileBytes,
-	}
-
-	attribution := _randomString(100000)
-
-	_, err = modifyStub(st, "ginormous="+attribution, "win")
-	if err == nil {
-		t.Errorf("expected an error, got: %s", reflect.TypeOf(err))
-	}
+		_, err = modifyStub(st, "ginormous="+attribution, "win")
+		if err == nil {
+			t.Error("Expected an error writing a huge attribution code")
+		}
+	})
 }
 
 func TestModifyStubDMG(t *testing.T) {
 	file, err := dmglib.OpenFile("../../testdata/attributable.dmg")
 	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+		t.Errorf("Unexpected error: %s", err)
 	}
 	defer file.Close()
 
 	dmg, err := file.Parse()
 	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+		t.Errorf("Unexpected error: %s", err)
 	}
+	t.Run("modifyStub - DMG success", func(t *testing.T) {
+		st := &stub{
+			body: dmg.Data,
+		}
 
-	st := &stub{
-		body: dmg.Data,
-	}
+		_, err = modifyStub(st, "hello=attribution&os=osx", "osx")
+		if err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+	})
 
-	_, err = modifyStub(st, "hello=attribution&os=osx", "osx")
-	if err != nil {
-		t.Errorf("modifyStub error: %s", err)
-	}
-}
+	t.Run("modifyStub - DMG parse failure", func(t *testing.T) {
+		st := &stub{
+			body: []byte("This is not a dmg!"),
+		}
+		_, err := modifyStub(st, "hello=errors", "osx")
 
-func TestModifyStubFailDMGWrite(t *testing.T) {
-	file, err := dmglib.OpenFile("../../testdata/attributable.dmg")
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-	defer file.Close()
+		if err == nil {
+			t.Error("Expected an error failing to parse")
+		}
+	})
 
-	dmg, err := file.Parse()
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
+	t.Run("modifyStub - DMG writing failure", func(t *testing.T) {
+		st := &stub{
+			body: dmg.Data,
+		}
+		attribution := makeRandomString(100000)
 
-	st := &stub{
-		body: dmg.Data,
-	}
-
-	attribution := _randomString(100000)
-
-	_, err = modifyStub(st, "ginormous="+attribution, "osx")
-	if err == nil {
-		t.Errorf("expected an error, got: %s", reflect.TypeOf(err))
-	}
-}
-
-func TestModifyStubFailDMGBody(t *testing.T) {
-	st := &stub{
-		body: []byte("This is not a dmg!"),
-	}
-	_, err := modifyStub(st, "hello=errors", "osx")
-
-	if err == nil {
-		t.Errorf("expected an error, got: %s", reflect.TypeOf(err))
-	}
+		_, err = modifyStub(st, "ginormous="+attribution, "osx")
+		if err == nil {
+			t.Error("Expected an error writing a huge attribution code")
+		}
+	})
 }
 
 func TestModifyStubFailOS(t *testing.T) {
@@ -163,6 +147,6 @@ func TestModifyStubFailOS(t *testing.T) {
 	_, err := modifyStub(st, "hello=errors", "ardweeno")
 
 	if err == nil {
-		t.Errorf("expected an error, got: %s", reflect.TypeOf(err))
+		t.Error("Expected an error for unsupported OS")
 	}
 }
