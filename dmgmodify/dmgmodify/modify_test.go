e567c71aac20b477743818ef93ddbd011bf05d4f
Ben Hearsum <ben@mozilla.com>
2023-12-12 11:14:43 -0500

    Override existing attribution codes when attributing DMGs

--- dmgmodify/dmgmodify/modify_test.go
+++ dmgmodify/dmgmodify/modify_test.go
@@ -13,58 +13,65 @@ import (
 )
 
 func TestWriteAttributionCode(t *testing.T) {
-	file, err := dmglib.OpenFile("../../testdata/attributable.dmg")
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-	defer file.Close()
+	for _, tc := range []struct {
+		testfile string
+	}{
+		{testfile: "../../testdata/attributable.dmg"},
+		{testfile: "../../testdata/attributable-with-existing-data.dmg"},
+	} {
+		file, err := dmglib.OpenFile(tc.testfile)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+		defer file.Close()
 
-	dmg, err := file.Parse()
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
+		dmg, err := file.Parse()
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
 
-	newCode := []byte("updated attribution code")
+		newCode := []byte("updated attribution code")
 
-	err = WriteAttributionCode(dmg, newCode)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
+		err = WriteAttributionCode(dmg, newCode)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
 
-	// Ensure the updated dmg contains the new attribution code
-	if !bytes.Contains(dmg.Data, newCode) {
-		t.Error("updated dmg data does not contain new attribution code!")
-	}
+		// Ensure the updated dmg contains the new attribution code
+		if !bytes.Contains(dmg.Data, newCode) {
+			t.Error("updated dmg data does not contain new attribution code!")
+		}
 
-	// Ensure the updated dmg can be parsed
-	newDmg, err := dmglib.ParseDMG(bytes.NewReader(dmg.Data))
-	if err != nil {
-		t.Errorf("updated dmg data cannot be parsed, got error: %s", err)
-	}
+		// Ensure the updated dmg can be parsed
+		newDmg, err := dmglib.ParseDMG(bytes.NewReader(dmg.Data))
+		if err != nil {
+			t.Errorf("updated dmg data cannot be parsed, got error: %s", err)
+		}
 
-	// Verify that the newly parsed data matches the modified in-memory data.
-	if !reflect.DeepEqual(dmg, newDmg) {
-		t.Errorf("updated dmg is not the same as its source after being reparsed!")
-	}
+		// Verify that the newly parsed data matches the modified in-memory data.
+		if !reflect.DeepEqual(dmg, newDmg) {
+			t.Errorf("updated dmg is not the same as its source after being reparsed!")
+		}
 
-	// Compare the hash against what we expect it to be
-	f, err := os.Open("../../testdata/attributed.dmg")
-	expectedHash := sha256.New()
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
+		// Compare the hash against what we expect it to be
+		f, err := os.Open("../../testdata/attributed.dmg")
+		expectedHash := sha256.New()
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
 
-	_, err = io.Copy(expectedHash, f)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-	expectedHex := hex.EncodeToString(expectedHash.Sum(nil))
+		_, err = io.Copy(expectedHash, f)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+		expectedHex := hex.EncodeToString(expectedHash.Sum(nil))
 
-	newHash := sha256.Sum256(newDmg.Data)
-	newHex := hex.EncodeToString(newHash[:])
+		newHash := sha256.Sum256(newDmg.Data)
+		newHex := hex.EncodeToString(newHash[:])
 
-	if expectedHex != newHex {
-		t.Errorf("wrong hash for updated dmg: %s, expected: %s", newHex, expectedHex)
+		if expectedHex != newHex {
+			t.Errorf("wrong hash for updated dmg: %s, expected: %s", newHex, expectedHex)
+		}
 	}
 }
 
